bdc4d5c79074dc375454ef1c89d61a938e2c6e6a
Spinal7 <ernso7@gmail.com>
2017-03-15 21:57:53 -0400

    Git Comment

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FirstForm
+{
+    partial class FirstForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FirstForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTaxPercent = new System.Windows.Forms.Label();
+            this.txtPurchase = new System.Windows.Forms.TextBox();
+            this.txtTotalDue = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Buxton Sketch", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tax Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Buxton Sketch", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Buxton Sketch", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tax Percentage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Buxton Sketch", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total Due";
+            // 
+            // lblTaxPercent
+            // 
+            this.lblTaxPercent.AutoSize = true;
+            this.lblTaxPercent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(192)))));
+            this.lblTaxPercent.ForeColor = System.Drawing.Color.LightBlue;
+            this.lblTaxPercent.Location = new System.Drawing.Point(294, 140);
+            this.lblTaxPercent.Name = "lblTaxPercent";
+            this.lblTaxPercent.Size = new System.Drawing.Size(47, 19);
+            this.lblTaxPercent.TabIndex = 4;
+            this.lblTaxPercent.Text = "7.5%";
+            // 
+            // txtPurchase
+            // 
+            this.txtPurchase.Location = new System.Drawing.Point(288, 89);
+            this.txtPurchase.Name = "txtPurchase";
+            this.txtPurchase.Size = new System.Drawing.Size(100, 26);
+            this.txtPurchase.TabIndex = 5;
+            this.txtPurchase.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTotalDue
+            // 
+            this.txtTotalDue.Enabled = false;
+            this.txtTotalDue.Location = new System.Drawing.Point(288, 182);
+            this.txtTotalDue.Name = "txtTotalDue";
+            this.txtTotalDue.Size = new System.Drawing.Size(100, 26);
+            this.txtTotalDue.TabIndex = 6;
+            this.txtTotalDue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Navy;
+            this.btnCalculate.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.ForeColor = System.Drawing.Color.Yellow;
+            this.btnCalculate.Location = new System.Drawing.Point(180, 248);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 37);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Compute";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // FirstForm
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(473, 383);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtTotalDue);
+            this.Controls.Add(this.txtPurchase);
+            this.Controls.Add(this.lblTaxPercent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(30, 30);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.Name = "FirstForm";
+            this.Text = "Spinal First Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FirstForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTaxPercent;
+        private System.Windows.Forms.TextBox txtPurchase;
+        private System.Windows.Forms.TextBox txtTotalDue;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstForm
+{
+    public partial class FirstForm : Form
+    {
+        public FirstForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Hope you are having fun! With GIT");
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string inValue;
+            double purchaseAmount;
+            double percent;
+            double ans;
+
+            while (double.TryParse(txtPurchase.Text, out purchaseAmount) == false)
+            {
+                MessageBox.Show("Value entered must be numeric");
+                txtPurchase.Text = "0.0";
+                //txtPurchase.Clear();
+                txtPurchase.Focus(); //Places the cursor inside the txtPurchase label
+            }
+
+            inValue = lblTaxPercent.Text;
+            inValue = inValue.Remove(inValue.Length - 1, 1);
+            percent = double.Parse(inValue) / 100;
+            ans = (purchaseAmount * percent) + purchaseAmount;
+            txtTotalDue.Text = string.Format("{0:C}", ans);
+        }
+    }
+}
